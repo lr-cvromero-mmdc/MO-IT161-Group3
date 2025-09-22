d6feaeae2ecb579194e9c1cc3c54cb1369f45672
Chris Vincent Romero <131778628+lr-cvromero-mmdc@users.noreply.github.com>
2025-09-23 01:07:05 +0800

    initial-commit-actual (#1) (#2)
    initial-commit-actual

---
+++ src/Footer.jsx
@@ -0,0 +1,17 @@
+
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2023 Car Wash. All rights reserved.</p>
+            <p>Follow us on social media:</p>
+            <ul>
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Instagram</a></li>
+            </ul>       
+        </footer>
+    );
+}
+
+export default Footer;
