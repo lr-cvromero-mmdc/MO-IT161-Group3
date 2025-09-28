df53696e08adcfb8f96f7349a35e47124a62229d
Chris Vincent Romero <lr.cvromero@mmdc.mcl.edu.ph>
2025-09-28 21:37:35 +0800

    initial-code-commit-to-rolling

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from "react-router-dom"
+import { Header } from "@/components/layout/Header"
+import { Footer } from "@/components/layout/Footer"
+import { SkipToContent } from "@/components/layout/SkipToContent"
+import { Home } from "@/pages/Home"
+import { About } from "@/pages/About"
+import { Services } from "@/pages/Services"
+import { Locations } from "@/pages/Locations"
+
+// Main application component
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <SkipToContent />
+      <Header />
+      <main id="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/locations" element={<Locations />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+// Exporting the App component as default
+export default App
