bed138f60eb5b53792069dc9facc5136c9baae37
Chris Vincent Romero <lr.cvromero@mmdc.mcl.edu.ph>
2025-11-05 01:18:20 +0800

    Update alias path in Vite config
    Changed the '@' alias in vite.config.ts to point to the project root instead of the src directory.

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   base: '/MO-IT161-Group3/'
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src/'),
+      '@': path.resolve(__dirname, './'),
     },
   },
 })
