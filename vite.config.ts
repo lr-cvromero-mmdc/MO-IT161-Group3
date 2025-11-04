c297e7257a79906a0750be54420c88e2ddc32923
Chris Vincent Romero <lr.cvromero@mmdc.mcl.edu.ph>
2025-11-05 01:45:37 +0800

    update-gh-mechanisms

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MO-IT161-Group3/',
+  base: "/MO-IT161-Group3",
   //resolve:
   //{
   //  alias: {
